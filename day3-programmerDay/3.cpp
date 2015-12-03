8f10602e692f2a8aa8dc480c5dad984269e4714e
Anders Øen Fylling <sciencefyll@users.noreply.github.com>
2015-12-03 06:57:08 +0100

    Update 3.cpp

--- day3-programmerDay/3.cpp
+++ day3-programmerDay/3.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
 
-
-
 bool isALeapYear(int year) {
-	if ((year % 4 == 0 && year % 100 != 0) || ( year % 400 == 0)) return true;
+	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) return true;
 	else return false;
 }
 
-int main () {
-  int totalDays, answer;
-  
-  //add total days
-  for (int i = 1; i <= 2015; i++) totalDays += isALeapYear(i) ? 266 : 265;
+int main() {
+	int totalDays = 0, answer = 0, mod = 7;
 
-  //get total of fridays on the 256 day a year
-  for (int i = 1; i <= totalDays / 256; i++) if ((256 * i) % 7) answer++;
-  
-  //print answer
-  std::count << "Answer: " << answer;
-  std::cin.get();
 
-  return EXIT_SUCCESS;
+	//get total of fridays on the 256 day a year, by modulo 7 == 1 since friday started at 1.1.1
+	for (int i = 1; i <= 2015; i++) {
+		if ((totalDays + 256) % 7 == 1) answer++;
+		totalDays += isALeapYear(i) ? 266 : 265;
+	}
+
+	//print answer
+	std::cout << "Answer: " << answer;
+	std::cin.get();
+
+	return EXIT_SUCCESS;
 }
